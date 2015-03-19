07e0196997de118b903b223e28a14bdc51c0dc9c
Armin Vieweg <Armin.Vieweg@sunzinet.com>
2015-03-19 13:19:03 +0100

    [TASK] Use closure in ext_localconf

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,30 +3,36 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'PwCommentsTeam.' . $_EXTKEY,
-	'Pi1',
-	array(
-		'Comment' => 'index,new,create,upvote,downvote',
-	),
-	array(
-		'Comment' => 'index,new,create,upvote,downvote',
-	)
-);
+$boot = function($extensionKey) {
+	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+		'PwCommentsTeam.' . $extensionKey,
+		'Pi1',
+		array(
+			'Comment' => 'index,new,create,upvote,downvote',
+		),
+		array(
+			'Comment' => 'index,new,create,upvote,downvote',
+		)
+	);
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'PwCommentsTeam.' . $_EXTKEY,
-	'Pi2',
-	array(
-		'Comment' => 'sendAuthorMailWhenCommentHasBeenApproved',
-	),
-	array(
-		'Comment' => 'sendAuthorMailWhenCommentHasBeenApproved',
-	)
-);
+	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+		'PwCommentsTeam.' . $extensionKey,
+		'Pi2',
+		array(
+			'Comment' => 'sendAuthorMailWhenCommentHasBeenApproved',
+		),
+		array(
+			'Comment' => 'sendAuthorMailWhenCommentHasBeenApproved',
+		)
+	);
 
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['extbase']['extensions']['PwComments']['modules'] = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['extbase']['extensions']['PwComments']['plugins'];
+	$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['extbase']['extensions']['PwComments']['modules']
+		= $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['extbase']['extensions']['PwComments']['plugins'];
 
-	// After save hook
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] =
-	'PwCommentsTeam\PwComments\Hooks\ProcessDatamap';
+		// After save hook
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] =
+		'PwCommentsTeam\PwComments\Hooks\ProcessDatamap';
+};
+
+$boot($_EXTKEY);
+unset($boot);
