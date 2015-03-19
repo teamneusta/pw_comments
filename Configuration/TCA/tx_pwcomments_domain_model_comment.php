75feb2b55e16aba9849503bf272cab9db652e4ac
Armin Vieweg <Armin.Vieweg@sunzinet.com>
2015-03-19 14:52:43 +0100

    [TASK] Codestyle

--- Configuration/TCA/tx_pwcomments_domain_model_comment.php
+++ Configuration/TCA/tx_pwcomments_domain_model_comment.php
@@ -58,7 +58,8 @@ return array(
 					array('', 0)
 				),
 				'foreign_table' => 'tx_pwcomments_domain_model_comment',
-				'foreign_table_where' => 'AND tx_pwcomments_domain_model_comment.uid=###REC_FIELD_l18n_parent### AND tx_pwcomments_domain_model_comment.sys_language_uid IN (-1,0)'
+				'foreign_table_where' => 'AND tx_pwcomments_domain_model_comment.uid=###REC_FIELD_l18n_parent###' .
+					' AND tx_pwcomments_domain_model_comment.sys_language_uid IN (-1,0)'
 			)
 		),
 		'l18n_diffsource' => array(
