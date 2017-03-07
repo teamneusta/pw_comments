a5c560127ba1adb3d79c0a2aecec85fa3383a481
Armin Vieweg <armin@v.ieweg.de>
2017-03-07 21:36:46 +0100

    [TASK] Fix ext_emconf.php after rebasing

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,37 +5,29 @@
  *  |
  *  | (c) 2011-2016 Armin Ruediger Vieweg <armin@v.ieweg.de>
  *  |     2015 Dennis Roemmich <dennis@roemmich.eu>
+ *  |     2016 Christian Wolfram <c.wolfram@chriwo.de>
  */
 
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'pwComments',
-	'description' => 'Powerful extension for providing comments, including replies on comments and voting.',
-	'category' => 'plugin',
-	'author' => 'Armin Ruediger Vieweg',
-	'author_email' => 'armin@v.ieweg.de',
-	'author_company' => '',
-	'shy' => '',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'module' => '',
-	'state' => 'stable',
-	'internal' => '',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
-	'lockType' => '',
-	'version' => '4.0.0-dev',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '6.2.0-7.2.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	'suggests' => array(
-	),
-);
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'pwComments',
+    'description' => 'Powerful extension for providing comments, including replies on comments and voting.',
+    'category' => 'plugin',
+    'author' => 'Armin Vieweg',
+    'author_email' => 'armin@v.ieweg.de',
+    'author_company' => '',
+    'state' => 'stable',
+    'uploadfolder' => false,
+    'createDirs' => null,
+    'modify_tables' => '',
+    'clearCacheOnLoad' => false,
+    'version' => '4.0.0-dev',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '7.6.0-8.9.99'
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ]
+    ]
+];
