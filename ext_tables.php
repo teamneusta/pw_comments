0a0b0ab29daacb6018a0655814fab457c9891c74
Armin Vieweg <armin@v.ieweg.de>
2018-07-10 21:00:37 +0200

    [BUGFIX] Fix wrong vendor prefix

--- ext_tables.php
+++ ext_tables.php
@@ -14,7 +14,7 @@ if (!defined('TYPO3_MODE')) {
 
 $boot = function ($extensionKey) {
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-        'InstituteWeb.' . $extensionKey,
+        'PwCommentsTeam.' . $extensionKey,
         'Pi1',
         'LLL:EXT:pw_comments/Resources/Private/Language/locallang_db.xlf:plugin.title'
     );
