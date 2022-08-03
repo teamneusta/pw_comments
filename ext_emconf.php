ebe1d7a7e4e3046a62c1e090ad49b2a17eef8867
Armin Vieweg <armin@v.ieweg.de>
2022-08-03 18:46:35 +0200

    [RELEASE] Version 5.2.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,10 +20,10 @@ $EM_CONF[$_EXTKEY] = [
     'createDirs' => null,
     'modify_tables' => '',
     'clearCacheOnLoad' => false,
-    'version' => '5.1.0',
+    'version' => '5.2.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-10.4.99'
+            'typo3' => '10.4.0-11.5.99'
         ],
         'conflicts' => [
         ],
