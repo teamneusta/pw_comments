2d0f1463eacc965d1efd8f4a2ad29c794b6b13b6
BobMali <BobMali@users.noreply.github.com>
2024-05-28 11:21:12 +0200

    [chore] update versions for release

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,17 +6,18 @@
  *  | (c) 2011-2022 Armin Vieweg <armin@v.ieweg.de>
  *  |     2015 Dennis Roemmich <dennis@roemmich.eu>
  *  |     2016-2017 Christian Wolfram <c.wolfram@chriwo.de>
+ *  |     2023-2024 Malek Olabi <m.olabi@neusta.de>
  */
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Comments for TYPO3 CMS',
     'description' => 'Powerful extension for providing comments, including replies on comments and voting.',
     'category' => 'plugin',
-    'author' => 'Armin Vieweg',
-    'author_email' => 'armin@v.ieweg.de',
+    'author' => 'Malek Olabi',
+    'author_email' => 'm.olabi@neusta.de',
     'author_company' => '',
     'state' => 'stable',
-    'version' => '6.0.0',
+    'version' => '6.0.1',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-12.99.99'
